07e85011945ed61d5a298898741d8d8d73cdc4e2
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-01-06 15:29:45 +0200

    fix

--- goit-js-hw-12-countries/src/fetchCountries.js
+++ goit-js-hw-12-countries/src/fetchCountries.js
@@ -1,6 +1,13 @@
 function fetchCountries(searchQuery)  {
    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
-     return fetch(url).then(resp => resp.json());
+   return fetch(url)
+      .then(resp => {
+         if (searchQuery) {
+            return resp.json()
+         } else {
+            return
+         }
+      });
 }
  
    export default fetchCountries;
--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -11,27 +11,27 @@ const inputRef = document.querySelector('.serchCauntries')
 const listRef = document.querySelector('.articles')
    
 inputRef.addEventListener('input', debounce(event => {
-    const inputValue = event.target.value
+    event.target.value
+        ?
+        fetchCountries(event.target.value).then(updateCountries)
+        :
+        listRef.innerHTML = '';
     
-         fetchCountries(inputValue).then(updateCountries)
-
-    },500))
+         },500))
 
 function updateCountries(countrie) {
-    const markupTittle = countrie.map(elem => `<li>${ elem.name }</li>`)
-    
     listRef.innerHTML = '';
     if (countrie.length < 10 && countrie.length > 1) {
-       
+       const markupTittle = countrie.map(elem => `<li>${ elem.name }</li>`)
         listRef.insertAdjacentHTML('beforeend', ...markupTittle.join(''))
     }
       
     else if (countrie.length === 1) {
        const markup = countriesTpl(countrie)
         listRef.insertAdjacentHTML('beforeend', markup)
-        } 
+    } 
     else {
-        
+      
         error({
             text: 'Too many matches found.Please enter a more specific query!',
             hide: false,
@@ -43,10 +43,3 @@ function updateCountries(countrie) {
     }
 }
 
-class ServisSerchCountie(countrie) {
-    updateCountries(countrie) {
-        
-},
-
-
-}
