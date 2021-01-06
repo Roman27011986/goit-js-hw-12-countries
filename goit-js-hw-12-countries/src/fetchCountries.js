cca9caebd653fafb9cb21018693fa3400edaa354
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-01-07 00:25:34 +0200

    fix

--- goit-js-hw-12-countries/src/fetchCountries.js
+++ goit-js-hw-12-countries/src/fetchCountries.js
@@ -1,13 +1,8 @@
 function fetchCountries(searchQuery)  {
    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
    return fetch(url)
-      .then(resp => {
-         if (searchQuery) {
-            return resp.json()
-         } else {
-            return
-         }
-      });
+      .then(resp => resp.json())
+        
 }
  
    export default fetchCountries;
