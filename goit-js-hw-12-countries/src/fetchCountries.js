742943176c6cc463ed030acf86463740a1268ea2
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-01-06 13:54:55 +0200

    ашы

--- goit-js-hw-12-countries/src/fetchCountries.js
+++ goit-js-hw-12-countries/src/fetchCountries.js
@@ -1,6 +1,6 @@
 function fetchCountries(searchQuery)  {
    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
-     return fetch(url).then(resp => resp.json()).then(data => data);
+     return fetch(url).then(resp => resp.json());
 }
  
    export default fetchCountries;
--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -4,7 +4,7 @@ import { error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import fetchCountries from './fetchCountries';
 import countriesTpl from './countries.hbs'
-import tittleTpl from './tittle.hbs'
+
 
 const debounce = require('lodash.debounce');
 const inputRef = document.querySelector('.serchCauntries')
@@ -12,16 +12,18 @@ const listRef = document.querySelector('.articles')
    
 inputRef.addEventListener('input', debounce(event => {
     const inputValue = event.target.value
-    if (inputValue) {
+    
          fetchCountries(inputValue).then(updateCountries)
-}
+
     },500))
 
 function updateCountries(countrie) {
+    const markupTittle = countrie.map(elem => `<li>${ elem.name }</li>`)
+    
     listRef.innerHTML = '';
     if (countrie.length < 10 && countrie.length > 1) {
-       const markup = tittleTpl(countrie)
-        listRef.insertAdjacentHTML('beforeend', markup)
+       
+        listRef.insertAdjacentHTML('beforeend', ...markupTittle.join(''))
     }
       
     else if (countrie.length === 1) {
@@ -41,3 +43,10 @@ function updateCountries(countrie) {
     }
 }
 
+class ServisSerchCountie(countrie) {
+    updateCountries(countrie) {
+        
+},
+
+
+}
